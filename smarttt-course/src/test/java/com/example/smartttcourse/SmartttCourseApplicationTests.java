3c0b75b950436439df41ea543065304c16b7658d
lunyasen <1668260406@qq.com>
2024-10-25 14:36:41 +0800

    .

--- smarttt-course/src/test/java/com/example/smartttcourse/SmartttCourseApplicationTests.java
+++ smarttt-course/src/test/java/com/example/smartttcourse/SmartttCourseApplicationTests.java
@@ -11,21 +11,5 @@ import static com.example.smartttcourse.Utils.CommonFunctions.generateEnhancedID
 
 @SpringBootTest
 class SmartttCourseApplicationTests {
-    @Autowired
-    private Mapper mapper;
-    @Test
-    void insertClassroomStudent(){
-        int rowNo = 0;
-        String classroomId = "8aea800182e80d000182e886980c0d7a";
-        String Obsid = "1597933787-318708d3-4f8d-4def-8f09-6a856de9237f";
-        List<CmClassroomStudent> cmClassroomStudents = mapper.getStudent(Obsid);
-        for( CmClassroomStudent cmClassroomStudent :cmClassroomStudents){
-            cmClassroomStudent.setId(generateEnhancedID("cm_classroom_student"));
-            cmClassroomStudent.setClassroomId(classroomId);
-            cmClassroomStudent.setRowNo(++rowNo);
-            mapper.insertStudent(cmClassroomStudent);
-        }
-
-    }
 
 }
