b9feb2a560dc9340738452bedad512cc00d173b2
wns666 <1207637937@qq.com>
2025-04-14 11:18:58 +0800

    2025/4/14 用户-角色新增学期字段

--- smarttt-course/src/main/java/com/example/smartttcourse/mapper/StUsersMapper.java
+++ smarttt-course/src/main/java/com/example/smartttcourse/mapper/StUsersMapper.java
@@ -18,7 +18,7 @@ public interface StUsersMapper{
             "WHERE t.obsid = #{obsid}\n")
     List<ResponsiblePerson> getAllTeacherByObsID(String id);
 
-    @Insert("insert into st_roleuser(id, userid, roleid, obsid, obsdeep, createtime) values (#{id}, #{userid}, #{roleid}, #{obsid}, #{obsdeep}, #{createtime})")
+    @Insert("insert into st_roleuser(id, userid, roleid, obsid, obsdeep, createtime,by1) values (#{id}, #{userid}, #{roleid}, #{obsid}, #{obsdeep}, #{createtime},#{by1})")
     void createOneRoleUser(StRoleUser stRoleUser);
 
     @Delete("delete from st_roleuser where obsid = #{obsid} and userid = #{userid} and roleid = #{roleid}")
