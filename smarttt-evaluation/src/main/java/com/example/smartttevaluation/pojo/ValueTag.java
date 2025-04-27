a0813013d7a2f0dedcbd4d71c5c8064f41260570
lunyasen <1668260406@qq.com>
2025-04-27 19:20:40 +0800

    修改id在传输中丢失精度问题

--- smarttt-evaluation/src/main/java/com/example/smartttevaluation/pojo/ValueTag.java
+++ smarttt-evaluation/src/main/java/com/example/smartttevaluation/pojo/ValueTag.java
@@ -3,6 +3,8 @@ package com.example.smartttevaluation.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,7 @@ import java.time.LocalDateTime;
 @Data
 public class ValueTag {
     @TableId(type = IdType.ASSIGN_ID)
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
     private String name;
     private String remark;
--- smarttt-evaluation/src/main/java/com/example/smartttevaluation/pojo/ValueType.java
+++ smarttt-evaluation/src/main/java/com/example/smartttevaluation/pojo/ValueType.java
@@ -2,6 +2,8 @@ package com.example.smartttevaluation.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,6 +11,7 @@ import java.time.LocalDateTime;
 @Data
 public class ValueType {
     @TableId(type = IdType.ASSIGN_ID)
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
     private String name;
     private String remark;
